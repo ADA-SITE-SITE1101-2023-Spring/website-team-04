226e5629ff37b91e9018d1fd73a64f13441af89a
SamilAliyev <130223628+SamilAliyev@users.noreply.github.com>
2023-04-16 19:40:37 +0400

    Delete js directory

--- js/hoc.js
+++
@@ -1,5 +0,0 @@
-function toggleVisibility() {
-  // const element = document.getElementById('gallery-main');
-  const gallery = document.getElementById('gallery');
-  gallery.classList.toggle('hidden');
-}
