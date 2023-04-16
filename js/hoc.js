4a57652d182d368d25669ef6f0535a4766cb6bf4
SamilAliyev <130223628+SamilAliyev@users.noreply.github.com>
2023-04-16 19:32:53 +0400

    Add files via upload

---
+++ js/hoc.js
@@ -0,0 +1,5 @@
+function toggleVisibility() {
+  // const element = document.getElementById('gallery-main');
+  const gallery = document.getElementById('gallery');
+  gallery.classList.toggle('hidden');
+}
